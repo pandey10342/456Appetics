398b38d90701d42b5284f33f735f24c0cd227c75
unknown <rohitp@techepoch.com>
2020-04-27 20:42:15 +0530

    ad issues

--- node_modules/cordova-plugin-admobpro/test/admob_simple.js
+++ node_modules/cordova-plugin-admobpro/test/admob_simple.js
@@ -3,9 +3,9 @@ var admobid = {};
 // TODO: replace the following ad units with your own
 if( /(android)/i.test(navigator.userAgent) ) {
   admobid = { // for Android
-    banner: 'ca-app-pub-3940256099942544/6300978111',
-    interstitial: 'ca-app-pub-3940256099942544/1033173712',
-    rewardvideo: 'ca-app-pub-3940256099942544/5224354917',
+    banner: '',
+    interstitial: '',
+    rewardvideo: '',
   };
 } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
   admobid = { // for iOS
